00e5bca329de4a74e8b82adb9eb2d419f3ab6f62
George <georgeparker939@gmail.com>
2025-02-10 21:00:38 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Numerical Methods
+
+Runs various numerical methods in python.
+
+## Using the code
+
+Install dependencies
+
+```
+pip install -r requirements.txt
+```
+
+Import required file,
+
+```
+python import root_finding.py
+```
+
+```
+python import differential_equations.py
+```
+
+```
+python import linear_algebra.py
+```
---
+++ differential_equations.py
@@ -0,0 +1,112 @@
+from typing import Callable, Dict
+import numpy as np
+import numpy.typing as npt
+
+def _initialise_array(y_0: npt.ArrayLike) -> np.ndarray | float:
+    """
+    Helper Function.
+    Initializes an array from the input `y_0`. If `y_0` is a scalar, it returns it as a float.
+    Otherwise, it converts `y_0` to a NumPy array.
+    """
+    if isinstance(y_0, (float, int)):
+        return y_0
+    return np.array(y_0, dtype=float)
+
+
+def euler_method(
+    f: Callable, x_0: float, y_0: npt.ArrayLike, h: float, n: int
+) -> Dict[float, float | np.ndarray]:
+    """
+    Approximates the solution to a differential equation using Euler's method.
+
+    Args:
+        f: The derivative function.
+        x_0: The initial x-value.
+        y_0: The initial y-value(s).
+        h: The step size.
+        n: The number of steps.
+
+    Returns:
+        A dictionary mapping x-values to their corresponding y-values.
+    """
+
+    assert h > 0, "h must have a positive value."
+
+    x, y = x_0, _initialise_array(y_0)
+    sol = {x: y}
+
+    for _ in range(n):
+        y += h * f(x, y)
+        x += h
+        sol[x] = y
+
+    return sol
+
+
+def adams_bashforth(
+    f: Callable, x_0: float, y_0: npt.ArrayLike, h: float, n: int
+) -> Dict[float, float | np.ndarray]:
+    """
+    Approximates the solution to a differential equation using the Adams-Bashforth method.
+
+    Args:
+        f: The derivative function.
+        x_0: The initial x-value.
+        y_0: The initial y-value(s).
+        h: The step size.
+        n: The number of steps.
+
+    Returns:
+        A dictionary mapping x-values to their corresponding y-values.
+    """
+
+    assert h > 0, "h must have a positive value."
+
+    x, y = x_0, _initialise_array(y_0)
+    sol = {x: y}
+
+    y += h * f(x, y)
+    x += h
+    sol[x] = y
+
+    for _ in range(n - 1):
+        y += h * ((3 / 2) * f(x, sol[x]) - (1 / 2) * f(x - h, sol[x - h]))
+        x += h
+        sol[x] = y
+
+    return sol
+
+
+def runge_kutta(
+    f: Callable, x_0: float, y_0: npt.ArrayLike, h: float, n: int
+) -> Dict[float, float | np.ndarray]:
+    """
+    Approximates the solution to a differential equation using the Runge-Kutta method.
+
+    Args:
+        f: The derivative function.
+        x_0: The initial x-value.
+        y_0: The initial y-value(s).
+        h: The step size.
+        n: The number of steps.
+
+    Returns:
+        A dictionary mapping x-values to their corresponding y-values.
+    """
+
+    assert h > 0, "h must have a positive value."
+
+    x, y = x_0, _initialise_array(y_0)
+    sol = {x: y}
+
+    for _ in range(n):
+        k1 = f(x, y)
+        k2 = f(x + h / 2, y + h * k1 / 2)
+        k3 = f(x + h / 2, y + h * k2 / 2)
+        k4 = f(x + h, y + h * k3)
+
+        y += (h / 6) * (k1 + k2 + k3 + k4)
+        x += h
+        sol[x] = y
+
+    return sol
